50411b8c22c55d14d4ccc3e3191f155195ddab5e
lilian <you@example.com>
2021-03-12 10:10:08 +0300

    balance colors

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ class UI {
             this.balance.classList.remove('showGreen', 'showBlack');
             this.balance.classList.add('showRed');
         } else if (total > 0) {
-            this.balance.classList.remove('showRed', 'showBlack');
+            this.balance.classList.remove('showRed', 'showWhite');
             this.balance.classList.add('showGreen');
         } else if (total === 0) {
             this.balance.classList.remove('showRed', 'showGreen');
